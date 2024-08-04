d4ac1d317e123d2824e715e8be6cfbb515d60fc3
Sonyalex6 <prototype546@mail.ru>
2024-08-04 14:12:19 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,14 +1,28 @@
-def send_email(message,recipient,sender = "univerity.help@gmail.com"):
-    if("@"and(".com"or".ru"or".net")) not in (recipient or sender) or("@" or(".com"or"ru"or".net")) not in (recipient or sender):
-        print(f"Невозможно отправить письмо с адреса {sender}на адрес {recipient}")
-    elif recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
-    elif sender !="university.help@gmail.com":
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-        send_email('messagje','university.help@mail.com','@.com')
-        send_email('messagje','university.help@mail.com')
-        send_email('messagje','universi@.com')
-        send_email('messagje','universi@.co')
-        send_email('messagje','universi@.com')
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    a = False
+    b = False
+    c = False
+    global sender_
+    if sender_1 != '':
+        sender = sender_1
+    if '@' in sender and '@' in recipient:
+        a = True
+    if sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'):
+        b = True
+    if recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'):
+        c = True
+    if a == False or b == False or c == False:
+        print(f'Невозможно отправить письмо с адреса: {sender} на адрес: {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса: {sender} на адрес: {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса: {sender} на адрес: {recipient}')
+recipient = input('Введите адрес получателя: ')
+message = input('Введите адрес текст сообщения:')
+sender_1 = input('Введите адрес отправителя или нажмите ENTER для адреса по умолчанию: ')
+print('*************************************')
+print()
+
+send_email(message, recipient)
